e45e01a588d8754640dc878b50a6153ac819f61e
ScottKavalinas <36358126+scottkavalinas@users.noreply.github.com>
2021-11-05 14:15:45 -0600

    removed unused code

--- lithium.py
+++ lithium.py
@@ -1,10 +1,8 @@
-import csv
 from selenium import webdriver
 url ='https://ca.investing.com/commodities/lithium-carbonate-99-min-china-futures-historical-data'
 browser = webdriver.Chrome()
 browser.get(url)
 data = browser.find_element_by_id('curr_table')
-
 f = open("lithium.txt", "a")
 f.write(data.text)
 f.close()
